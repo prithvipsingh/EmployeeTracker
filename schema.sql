b33cbf6aebd3d4de7bc63a7b0abe15d92af83255
prithvipsingh <ppsdeora@gmail.com>
2020-05-09 14:57:41 +1000

    "create more tables"

--- schema.sql
+++ schema.sql
@@ -17,3 +17,19 @@ manager_id INTEGER(10) NULL,
 PRIMARY KEY(id)
 
 );
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL (30.2),
+  department_id INTEGER (10),
+  PRIMARY KEY (id)
+);
+CREATE TABLE departments (
+  id INTEGER AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM employees;
+SELECT * FROM roles;
+SELECT * FROM departments;
