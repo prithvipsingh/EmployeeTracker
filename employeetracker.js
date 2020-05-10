c3b098115aef1c2da1469404cccf924fdadb2075
prithvipsingh <ppsdeora@gmail.com>
2020-05-10 11:08:41 +1000

    adding connection with database

--- employeetracker.js
+++ employeetracker.js
@@ -1,1 +1,22 @@
 // seting up pakages which are need to run app
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// setting up connection with database 
+const connection = mysql.createConnection({
+
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    passoword: "Pps1187#deora",
+    database: "employee_DB"
+
+});
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start(); 
+  });
+
+
+  
