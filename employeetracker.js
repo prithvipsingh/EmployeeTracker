6bf75355d6560c0e02e8a0d87cf0484a06647f7f
prithvipsingh <ppsdeora@gmail.com>
2020-05-13 11:09:41 +1000

    adding view empoyees function

--- employeetracker.js
+++ employeetracker.js
@@ -12,10 +12,10 @@ const connection = mysql.createConnection({
 });
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId +"\n");
+  console.log("connected as id " + connection.threadId + "\n");
   start();
 });
-//creating start function which promot all options 
+//creating start function which promot all options
 function start() {
   inquirer
     .prompt([
@@ -76,43 +76,86 @@ function start() {
       }
     });
 }
- // create function for add new employee
- function addEmployee() {
+// create function for add new employee
+function addEmployee() {
   console.log("Inserting a new employee.\n");
-  inquirer 
-    .prompt ([ 
+  inquirer
+    .prompt([
       {
-        type: "input", 
+        type: "input",
         message: "First Name?",
         name: "first_name",
       },
       {
-        type: "input", 
+        type: "input",
         message: "Last Name?",
-        name: "last_name"
+        name: "last_name",
       },
       {
         type: "list",
         message: "What is the employee's role?",
-        name: "role_id", 
-        choices: [1,2,3]
+        name: "role_id",
+        choices: [1, 2, 3],
       },
       {
-        type: "input", 
+        type: "input",
         message: "Who is their manager?",
-        name: "manager_id"
-      }
+        name: "manager_id",
+      },
     ])
-    .then (function(res){
+    .then(function (res) {
       const query = connection.query(
-        "INSERT INTO employees SET ?", 
-       res,
-        function(err, res) {
+        "INSERT INTO employees SET ?",
+        res,
+        function (err, res) {
           if (err) throw err;
-          console.log( "Employee added!\n");
-  
-          start (); 
+          console.log("Employee added!\n");
+
+          start();
         }
-      );    
-    })
-  }
+      );
+    });
+}
+// Function for view all employees
+function viewAllEmployees() {
+  connection.query(
+    'SELECT employees.first_name, employees.last_name, roles.title AS "role", managers.first_name AS "manager" FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN employees managers ON employees.manager_id = managers.id GROUP BY employees.id',
+    function (err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.table(res);
+      start();
+    }
+  );
+}
+// Function for delete employees
+function removeEmployee() {
+  let employeeList = [];
+  connection.query(
+    "SELECT employees.first_name, employees.last_name FROM employees",
+    (err, res) => {
+      for (let i = 0; i < res.length; i++) {
+        employeeList.push(res[i].first_name + " " + res[i].last_name);
+      }
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            message: "Which employee would you like to delete?",
+            name: "employee",
+            choices: employeeList,
+          },
+        ])
+        .then(function (res) {
+          const query = connection.query(
+            `DELETE FROM employees WHERE concat(first_name, ' ' ,last_name) = '${res.employee}'`,
+            function (err, res) {
+              if (err) throw err;
+              console.log("Employee deleted!\n");
+              start();
+            }
+          );
+        });
+    }
+  );
+}
