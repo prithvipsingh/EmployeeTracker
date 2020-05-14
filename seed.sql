6d88d9f97f2f22db1218512bc9209f91a20408d7
prithvipsingh <ppsdeora@gmail.com>
2020-05-14 16:39:39 +1000

    update seed.sql file

--- seed.sql
+++ seed.sql
@@ -1,8 +1,53 @@
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Jason", "Smith", 1, 2), ("Ellen", "Park", 2, 1), ("Stacy", "Carter", 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jason", "Mark", 1, 2), ("Mohit", "Singh", 2, 1), ("Stacy", "Carter", 3, 2);
+INSERT into employee (first_name, last_name, role_id)
+values ("John", "Stamos", 3); 
+INSERT into employee (first_name, last_name, role_id)
+values ("Jim", "Baker", 4);
+INSERT into employee (first_name, last_name, role_id)
+values ("George", "Washington", 5);
+INSERT into employee (first_name, last_name, role_id)
+values ("Meghan", "Kelly", 6);
+INSERT into employee (first_name, last_name, role_id)
+values ("Tom", "Hanks", 7);
+INSERT into employee (first_name, last_name, role_id)
+values ("Dolly", "Parton", 8);
 
-INSERT INTO roles (title, salary, department_id)
+
+
+
+INSERT INTO role (title, salary, department_id)
 VALUES ("Manager", 200, 1), ("Engineer", 100, 2), ("Intern", 10, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 200, 1), ("Engineer", 100, 2), ("Intern", 10, 2);
+INSERT into role (title, salary, department_id)
+VALUES ("Sales Lead", 45000, 1);
+INSERT into role (title, salary, department_id)
+VALUES ("Salesperson", 35000, 1);
+INSERT into role (title, salary, department_id)
+VALUES ("Lead Engineer", 43000, 2);
+INSERT into role (title, salary, department_id)
+VALUES ("Accountant", 50000, 3);
+INSERT into role (title, salary, department_id)
+VALUES ("Legal", 50000, 4);
+INSERT into role (title, salary, department_id)
+VALUES ("Manager", 65000, 5);
+
+
 
 INSERT INTO department (name)
 VALUES ("Admin"), ("Engineering"), ("Education");
+INSERT INTO department (name)
+VALUES ("Admin"), ("Engineering"), ("Education");
+INSERT into department (name)
+VALUES ("Sales");
+INSERT into department (name)
+VALUES ("Engineering");
+INSERT into department (name)
+VALUES ("Finance");
+INSERT into department (name)
+VALUES ("Legal");
+INSERT into department (name)
+VALUES ("Manager");
