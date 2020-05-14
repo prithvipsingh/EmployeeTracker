54a73af9ce0f181607cd8414aba8cae055e80b22
prithvipsingh <ppsdeora@gmail.com>
2020-05-14 16:51:16 +1000

    Adding function for update employeerole

--- employeetracker.js
+++ employeetracker.js
@@ -271,39 +271,39 @@ function viewAllRoles() {
     }
   );
 }
-// Adding function for update employeerole
-// function updateEmployeeRole(){
-//   connection.query("SELECT first_name, last_name, id FROM employee",
-//   function(err,res){
-//     // for (let i=0; i <res.length; i++){
-//     //   employees.push(res[i].first_name + " " + res[i].last_name);
-//     // }
-//     let employee = res.map(employee => ({name: employee.first_name + " " + employee.last_name, value: employee.id}))
+//Adding function for update employeerole
+function updateEmployeeRole(){
+  connection.query("SELECT first_name, last_name, id FROM employee",
+  function(err,res){
+    // for (let i=0; i <res.length; i++){
+    //   employees.push(res[i].first_name + " " + res[i].last_name);
+    // }
+    let employee = res.map(employee => ({name: employee.first_name + " " + employee.last_name, value: employee.id}))
   
-//     inquirer
-//     .prompt([
-//       {
-//         type: "list",
-//         name: "employeeName",
-//         message: "Which employee's role would you like to update?", 
-//         choices: employee
-//       },
-//       {
-//         type: "input",
-//         name: "role",
-//         message: "What is your new role?"
-//       }
-//     ])
-//     .then (function(res){
-//       connection.query(`UPDATE employee SET role_id = ${res.role} WHERE id = ${res.employeeName}`,
-//       function (err, res){
-//         console.log(res);
-//         //updateRole(res);
-//         start();
-//       }
-//       )
-//     })
-//   }
-//   )
-//   }
+    inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employeeName",
+        message: "Which employee's role would you like to update?", 
+        choices: employee
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "What is your new role?"
+      }
+    ])
+    .then (function(res){
+      connection.query(`UPDATE employee SET role_id = ${res.role} WHERE id = ${res.employeeName}`,
+      function (err, res){
+        console.log(res);
+        //updateRole(res);
+        start();
+      }
+      )
+    })
+  }
+  )
+  }
   
